e4cc17dc151dbc84ea239bc6cfa71ab91d0201b4
will <w150037@163.com>
2023-03-31 19:01:44 +0800

    01-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...args) {
+    let result = [];
+    list.forEach(element => {
+        if(!args.includes(element)) {
+            result.push(element);
+        }
+    });
+    return result;
 };
 
 // Do not edit below this line
