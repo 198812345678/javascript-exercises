72df88f06c7c5ef04f0898a62eb795b15136796f
wwang7 <wwang7@leqee.com>
2023-04-07 18:38:52 +0800

    05-07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(!(typeof num1 === 'number') 
+        || !(typeof num2 === 'number')
+        || num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    let sum = 0;
+    for (let index = Math.min(num1, num2); index <= Math.max(num1, num2); index++) {
+        sum += index;
+    }
+
+    return sum;
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return roundHalfUp((f - 32) * (5/9), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return roundHalfUp(c * (9/5) + 32, 1);
 };
+
+function roundHalfUp(num, decimal) {
+  return Math.round(num * 10**decimal) / 10**decimal;
+}
 
 // Do not edit below this line
 module.exports = {
