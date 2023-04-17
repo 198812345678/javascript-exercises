41c5e9602cb3481fe3372275f60b2954410b6816
wwang7 <wwang7@leqee.com>
2023-04-17 14:20:22 +0800

    08 halfway

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
+const sum = function(...args) {
+  let sum = 0;
+  for (let i = 0; i < args.length; i++) {
+    sum += args[i];
+  }
+  return sum;
+  
+};
+
+const multiply = function(...args) {
+  let a = 1;
+  for (let i = 0; i < args.length; i++) {
+    a *= args[i];
+  }
+  return a;
+};
+
+const power = function(a, p) {
+  return a ^ p;
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+  
+  if (a < 0) {
+    return "undefined";
+  }
 
-const multiply = function() {
+  let result = 1;
+  for (let i = 1; i <= a; i++) {
+    result = result * i;
+  }
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  return result;
 };
 
 // Do not edit below this line
